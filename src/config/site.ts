60f1f8018f6dbaf14d439b8d10a7f55544c70f55
CodexLin <codexlin@gmail.com>
2024-08-19 09:29:46 +0800

    feat: theme and ui

---
+++ src/config/site.ts
@@ -0,0 +1,19 @@
+import { SiteConfig } from "~/types";
+
+import { env } from "~/env";
+
+export const siteConfig: SiteConfig = {
+  name: "CodexLin",
+  author: "CodexLin",
+  description:
+    "Next.js 14+ t3 starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
+  keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui", "t3"],
+  url: {
+    base: env.NEXT_PUBLIC_APP_URL,
+    author: "CodexLin",
+  },
+  links: {
+    github: "https://github.com/codexlin",
+  },
+  ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
+};
--- src/env.js
+++ src/env.js
@@ -20,6 +20,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_APP_URL: z.string().min(1), // NEXT_PUBLIC_APP_URL 必须是非空字符串
   },
 
   /**
@@ -29,6 +30,7 @@ export const env = createEnv({
   runtimeEnv: {
     POSTGRES_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL, // 从 process.env 中获取 NEXT_PUBLIC_APP_URL
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
